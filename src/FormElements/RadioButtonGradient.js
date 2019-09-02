807f57920f077d059952b49c2da601afa51339ca
DavidVorac <davevorac@gmail.com>
2019-09-02 13:40:03 +0200

    Gradient template for Radio Buttons

---
+++ src/FormElements/RadioButtonGradient.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const RadioButtonGradient = () => {
+  return (
+    <svg width="0" height="0">
+      <defs>
+        <linearGradient
+          x1="0%"
+          y1="0%"
+          x2="0%"
+          y2="100%"
+          id="radioButtonGradient"
+        >
+          <stop offset="0%" stop-color="#1875F0"></stop>
+          <stop offset="100%" stop-color="#3294FE"></stop>
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default RadioButtonGradient;
